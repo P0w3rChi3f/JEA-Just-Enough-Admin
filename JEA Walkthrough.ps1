fa441177d963498221f2438eaf30cdde2570d1cd
James Honeycutt <honeycutt.james@gmail.com>
2020-12-16 14:59:05 -0500

    converted Enter-Pssession to invoke-invoke command

--- JEA Walkthrough.ps1
+++ JEA Walkthrough.ps1
@@ -1,16 +1,35 @@
-﻿# Verify users and groups
+﻿#Build Credentials
 
-Enter-PSSession -ComputerName JEA-DC -Credential jeademo\jea.admin
+$admin = "jeademo\jea.admin"
+$contractor = "jeademo\jea.contractor"
+$developer = "jeademo\jea.dev"
+$user = "jeademo\jea.user" 
+
+$password = ConvertTo-SecureString -string "Password12#$" -AsPlainText -Force
+
+$adminCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $admin, $password
+$contractorCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $contractor, $password
+$developerCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $developer, $password
+$userCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $password
+
+
+# Verify users and groups
+Enter-PSSession -ComputerName JEA-DC -Credential $adminCreds
 
 Get-ADUser -filter 'name -like "JEA*"' | Select-Object name
 Get-ADGroupMember -Identity "Domain Admins" | Select-Object name
 Get-ADGroupMember "Domain Users" | Select-Object name
 
+Get-ADUser -Identity jea.contractor | Get-ADPrincipalGroupMembership
+Get-ADUser -Identity jea.dev | Get-ADPrincipalGroupMembership
+Get-ADUser -Identity jea.user | Get-ADPrincipalGroupMembership
+Get-ADUser -Identity jea.admin | Get-ADPrincipalGroupMembership
+
 #test remote sessions
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential jeademo\jea.user
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential jeademo\jea.contractor
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential jeademo\jea.dev
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential jeademo\jea.admin
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $userCreds
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $contractorCreds
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $developerCreds
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $adminCreds
 
 get-command * | Group-Object CommandType
 
@@ -19,7 +38,7 @@ Exit-PSSession
 
 #build the JEA Module Folder
 New-Item -ItemType directory -Path 'C:\JEAConfigs\Modules\' -Name ISSA
-New-Item -ItemType directory -Path 'C:\JEAConfigs\Modules\ISSA' -Name RoleCapabilities #Must Have
+New-Item -ItemType directory -Path 'C:\JEAConfigs\Modules\ISSA' -Name RoleCapabilities -force #Must Have
 
 #create a role capability file
 New-PSRoleCapabilityFile -Path 'C:\JEAConfigs\Modules\ISSA\RoleCapabilities\Developer.psrc'
@@ -44,42 +63,57 @@ New-PSSessionConfigurationFile -Path 'C:\JEAConfigs\Modules\ISSA\RestrictedSessi
 Copy-Item -Path 'C:\JEAConfigs\JEA-DemoFiles\RoleCapabilities\Developer.psrc' -Destination 'C:\JEAConfigs\Modules\ISSA\RoleCapabilities\Developer.psrc' -Force
 Copy-Item -Path 'C:\JEAConfigs\JEA-DemoFiles\RoleCapabilities\Contractors.psrc' -Destination 'C:\JEAConfigs\Modules\ISSA\RoleCapabilities\Contractors.psrc' -Force
 
-Enter-PSSession -ComputerName JEA-DemoSVR -Credential jeademo\jea.Admin
-Set-Location 'C:\Program Files\WindowsPowerShell\Modules\'
-Get-ChildItem .
+<#
+Enter-PSSession -ComputerName JEA-DemoSVR -Credential $adminCreds
+#Set-Location 'C:\Program Files\WindowsPowerShell\Modules\'
 Exit-PSSession
+#>
 
-$DemoSVRSession = New-PSSession -ComputerName Jea-DemoSVR -Credential 'jeademo\jea.admin'
+invoke-command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock {Get-ChildItem 'C:\Program Files\WindowsPowerShell\Modules\'}
+
+$DemoSVRSession = New-PSSession -ComputerName Jea-DemoSVR -Credential $adminCreds
 Copy-Item -Path 'C:\JEAConfigs\Modules\ISSA' -Destination 'c:\Program Files\WindowsPowerShell\Modules\ISSA' -ToSession $DemoSVRSession -Recurse
+
+<#
 Enter-PSSession $DemoSVRSession
 Set-Location 'C:\Program Files\WindowsPowerShell\Modules\'
 Get-ChildItem .
-##Exit-PSSession
+Exit-PSSession
+#>
+invoke-command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock {Get-ChildItem 'C:\Program Files\WindowsPowerShell\Modules\'}
 
 #Register the EndPoint - 
-
-Enter-PSSession -ComputerName JEA-DemoSVR -Credential jeademo\jea.Admin
+invoke-command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock {Get-PSSessionConfiguration | Select-Object name}
+<#
+Enter-PSSession -ComputerName JEA-DemoSVR -Credential $adminCreds
 Set-Location 'C:\Program Files\WindowsPowerShell\Modules\'
 Get-PSSessionConfiguration | Select-Object name
 Exit-PSSession
+#>
 
-Invoke-Command -ComputerName JEA-DemoSVR -Credential jeademo\jea.admin -ScriptBlock { Unregister-PSSessionConfiguration -Name ISSA-Demo }
-Invoke-Command -ComputerName JEA-DemoSVR -Credential jeademo\jea.admin -ScriptBlock { Register-PSSessionConfiguration -Name ISSA-Demo -Path 'C:\Program Files\WindowsPowerShell\Modules\ISSA\RestrictedSession.pssc' }
+
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock { Unregister-PSSessionConfiguration -Name ISSA-Demo }
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock { Register-PSSessionConfiguration -Name ISSA-Demo -Path 'C:\Program Files\WindowsPowerShell\Modules\ISSA\RestrictedSession.pssc' }
+
 
 # Restart WinRM Service on remote computer
+invoke-command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock {Get-PSSessionConfiguration | Select-Object name}
+<#
 Enter-PSSession -ComputerName JEA-DemoSVR -Credential jeademo\jea.Admin
 Get-PSSessionConfiguration | Select-Object name
 Exit-PSSession
+#>
 
-Invoke-Command -ComputerName JEA-DemoSVR -Credential jeademo\jea.Admin -ScriptBlock { Restart-Service -Name WinRM }
-Invoke-Command -ComputerName JEA-DemoSVR -Credential jeademo\jea.Admin -ScriptBlock { Get-PSSessionConfiguration | Select-Object name,permission }
 
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential 'jeademo\jea.user' -ConfigurationName ISSA-Demo
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock { Restart-Service -Name WinRM }
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock { Get-PSSessionConfiguration | Select-Object name,permission }
+
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $userCreds -ConfigurationName ISSA-Demo
 
 # Enter Remote Sessions with unprivledge users
 
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential 'jeademo\jea.dev'
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential 'jeademo\jea.dev' -ConfigurationName ISSA-Demo
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $developerCreds
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $developerCreds -ConfigurationName ISSA-Demo
 
 get-command * | Group-Object CommandType
 Restart-Service -Name BITS
@@ -88,25 +122,29 @@ whoami
 Exit-PSSession 
 
 # Make sure I am loged into the Jea-DEMOSVR
-Enter-PSSession -ComputerName JEA-DEMOSVR -Credential 'jeademo\jea.contractor' -ConfigurationName ISSA-Demo 
-Get-Command
-Restart-Service -Name BITS
-whoami
+Enter-PSSession -ComputerName JEA-DEMOSVR -Credential $contractorCreds -ConfigurationName ISSA-Demo 
+invoke-command -ComputerName JEA-DEMOSVR -Credential $contractorCreds -ConfigurationName ISSA-Demo -ScriptBlock {Restart-Service -Name BITS}
+invoke-command -ComputerName JEA-DEMOSVR -Credential $contractorCreds -ConfigurationName ISSA-Demo -ScriptBlock {Get-Command}
+invoke-command -ComputerName JEA-DEMOSVR -Credential $contractorCreds -ConfigurationName ISSA-Demo -ScriptBlock {whoami}
+invoke-command -ComputerName JEA-DEMOSVR -Credential $contractorCreds -ConfigurationName ISSA-Demo -ScriptBlock {restart-computer}
+
+<#
 restart-computer
 Restart-Computer -force
+#>
+
 
 ##################################################################################################
 # Reset WinRM Walk Through
 
-Invoke-Command -ComputerName JEA-DemoSVR -ScriptBlock { 
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock { 
     Unregister-PSSessionConfiguration -Name ISSA-Demo  
-
     Remove-Item -path 'c:\Program Files\WindowsPowerShell\Modules\ISSA' -Recurse -Force
-
-    Restart-Service -Name WinRM -Force
-
-    Get-PSSessionConfiguration | Select-Object name,permission 
-} -Credential jeademo\jea.admin
-
+    Restart-Service -Name WinRM -Force 
+} 
+Invoke-Command -ComputerName JEA-DemoSVR -Credential $adminCreds -ScriptBlock {
+    Get-PSSessionConfiguration | Select-Object name,permission | Format-Table
+    Get-ChildItem -path 'c:\Program Files\WindowsPowerShell\Modules\' | Format-Table
+}
 
 
